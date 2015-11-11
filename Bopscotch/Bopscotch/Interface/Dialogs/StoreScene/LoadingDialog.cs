8531ed93f98c0ba81f648f3dd220f39c29471794
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-11-11 20:39:06 +0000

    Loading dialog spinner

--- Bopscotch/Bopscotch/Interface/Dialogs/StoreScene/LoadingDialog.cs
+++ Bopscotch/Bopscotch/Interface/Dialogs/StoreScene/LoadingDialog.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.Xna.Framework;
 
+using Leda.Core;
+using Leda.Core.Asset_Management;
+
+using Bopscotch;
+
 namespace Bopscotch.Interface.Dialogs.StoreScene
 {
     public class LoadingDialog : ButtonDialog
     {
         public delegate void EntryCompleteHandler();
         private EntryCompleteHandler _entryCompleteHandler;
+        private float _spinnerRotation;
 
         public LoadingDialog(EntryCompleteHandler entryCompleteHandler)
             : base()
@@ -21,6 +27,8 @@ namespace Bopscotch.Interface.Dialogs.StoreScene
         {
             base.Reset();
             WorldPosition = new Vector2(0.0f, -Height);
+
+            _spinnerRotation = 0.0f;
         }
 
         protected override void HandleDialogEntryCompletion()
@@ -33,8 +41,32 @@ namespace Bopscotch.Interface.Dialogs.StoreScene
             }
         }
 
-        private const int Dialog_Height = 300;
+        public override void Update(int millisecondsSinceLastUpdate)
+        {
+            base.Update(millisecondsSinceLastUpdate);
+
+            _spinnerRotation -= MathHelper.ToRadians(Spin_Degrees_Per_Millisecond) * millisecondsSinceLastUpdate;
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+
+            spriteBatch.Draw(
+                TextureManager.Textures["load-spinner"],
+                GameBase.ScreenPosition(Definitions.Back_Buffer_Center.X, WorldPosition.Y + 175.0f),
+                null,
+                Color.White,
+                _spinnerRotation,
+                new Vector2(TextureManager.Textures["load-spinner"].Width, TextureManager.Textures["load-spinner"].Height) / 2.0f,
+                GameBase.ScreenScale(),
+                Microsoft.Xna.Framework.Graphics.SpriteEffects.None,
+                0.1f);
+        }
+
+        private const int Dialog_Height = 400;
         private const string Dialog_Caption = "loading";
+        private const float Spin_Degrees_Per_Millisecond = 0.1f;
         private const float Top_Line_Y = 80.0f;
         private const float Line_Height = 50.0f;
     }
