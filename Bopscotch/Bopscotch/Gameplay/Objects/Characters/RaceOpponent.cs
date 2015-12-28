754af172ffe8333cc627faec4efefcb39d08daa3
Ben-P-Leda <benp@ledaentertainment.co.uk>
2015-12-28 08:39:52 +0000

    Might have a workable version... :grin:

--- Bopscotch/Bopscotch/Gameplay/Objects/Characters/RaceOpponent.cs
+++ Bopscotch/Bopscotch/Gameplay/Objects/Characters/RaceOpponent.cs
@@ -9,21 +9,27 @@ using Leda.Core.Asset_Management;
 using Leda.Core.Motion;
 
 using Bopscotch.Communication;
+using Bopscotch.Gameplay.Coordination;
 
 namespace Bopscotch.Gameplay.Objects.Characters
 {
     public class RaceOpponent : ICameraRelative, ISimpleRenderable, IMobile
     {
         private long _millisecondsSinceLastComms;
-        private Vector2 _velocity;
+        private Vector2 _peerVelocity;
         private Vector2 _expectedPosition;
         private Vector2 _lastCommsPosition;
+        private int _lastCommsUpdateTime;
         private int _packetsAtCurrentPosition;
-        private bool _isAlive;
+        private int _restartDirection;
+        private Vector2 _clientVelocity;
+        private int _millisecondsToRestart;
+        private int _currentLap;
+        private int _currentCheckpoint;
 
         public Vector2 WorldPosition { get; set; }
         public bool WorldPositionIsFixed { get { return false; } }
-        public bool Visible { get { return true; } set { } }
+        public bool Visible { get; set; }
         public int RenderLayer { get { return 2; } set { } }
         public Vector2 CameraPosition { get; set; }
         public InterDeviceCommunicator Communicator { private get; set; }
@@ -35,19 +41,30 @@ namespace Bopscotch.Gameplay.Objects.Characters
 
         public void Reset()
         {
+            _currentLap = 0;
+            _currentCheckpoint = 0;
             _millisecondsSinceLastComms = 0;
-            _velocity = Vector2.Zero;
+            _peerVelocity = Vector2.Zero;
 
             WorldPosition = Vector2.Zero;
         }
 
-        public void SetForRaceStart(Vector2 startPosition)
+        public void SetForRaceStart(Vector2 startPosition, bool facingLeft)
         {
+            _restartDirection = (facingLeft ? -1 : 1);
             _expectedPosition = startPosition;
-            WorldPosition = startPosition;
+            _clientVelocity = Vector2.Zero;
 
             _lastCommsPosition = startPosition;
             _packetsAtCurrentPosition = 0;
+
+            WorldPosition = startPosition;
+            Visible = true;
+        }
+
+        public void StartMovement()
+        {
+            _clientVelocity = new Vector2(Player.PlayerMotionEngine.Minimum_Movement_Speed * _restartDirection, 0.0f);
         }
 
         public void Update(int millisecondsSinceLastUpdate)
@@ -55,19 +72,24 @@ namespace Bopscotch.Gameplay.Objects.Characters
             if (_millisecondsSinceLastComms > Communicator.MillisecondsSinceLastReceive)
             {
                 LogPositionUpdates();
+                UpdateProgress();
                 UpdateLifeState();
 
-                if (_isAlive)
+                if (Visible)
                 {
                     Vector2 step = ((Communicator.OtherPlayerData.PlayerWorldPosition - _expectedPosition) / _millisecondsSinceLastComms) * 0.95f;
-                    _velocity = (_velocity * 0.5f) + (step * 0.5f);
+                    _peerVelocity = (_peerVelocity * 0.5f) + (step * 0.5f);
                 }
             }
 
-            _expectedPosition += _velocity * millisecondsSinceLastUpdate;
+            CheckForRestart(millisecondsSinceLastUpdate);
+
+            _expectedPosition += _peerVelocity * millisecondsSinceLastUpdate;
             _millisecondsSinceLastComms = Communicator.MillisecondsSinceLastReceive;
 
-            WorldPosition = _expectedPosition;
+            UpdateClientVelocity();
+
+            WorldPosition += _clientVelocity * millisecondsSinceLastUpdate;
         }
 
         private void LogPositionUpdates()
@@ -75,6 +97,7 @@ namespace Bopscotch.Gameplay.Objects.Characters
             if (Communicator.OtherPlayerData.PlayerWorldPosition != _lastCommsPosition)
             {
                 _lastCommsPosition = Communicator.OtherPlayerData.PlayerWorldPosition;
+                _lastCommsUpdateTime = Communicator.OtherPlayerData.TotalRaceTimeElapsedInMilliseconds;
                 _packetsAtCurrentPosition = 0;
             }
             else
@@ -83,18 +106,60 @@ namespace Bopscotch.Gameplay.Objects.Characters
             }
         }
 
+        private void UpdateProgress()
+        {
+            if ((Communicator.OtherPlayerData.LapsCompleted > _currentLap) || (Communicator.OtherPlayerData.LastCheckpointIndex > _currentCheckpoint))
+            {
+                _currentCheckpoint = Communicator.OtherPlayerData.LastCheckpointIndex;
+                _currentLap = Communicator.OtherPlayerData.LapsCompleted;
+
+                _restartDirection = Math.Sign(_peerVelocity.X);
+            }
+        }
+
         private void UpdateLifeState()
         {
-            if ((_isAlive) && (_packetsAtCurrentPosition > 1))
+            if ((Visible) && (_packetsAtCurrentPosition > 1))
             {
-                _isAlive = false;
-                _velocity = Vector2.Zero;
+                _peerVelocity = Vector2.Zero;
+                _millisecondsToRestart = RaceProgressCoordinator.Race_Resurrect_Sequence_Duration_In_Milliseconds - (int)_millisecondsSinceLastComms;
+
+                Visible = false;
             }
-            else if ((!_isAlive) && (_packetsAtCurrentPosition < 1))
+            else if ((!Visible) && (_packetsAtCurrentPosition < 1))
             {
-                _isAlive = true;
                 _expectedPosition = Communicator.OtherPlayerData.PlayerWorldPosition;
+                _clientVelocity = Vector2.Zero;
+
                 WorldPosition = Communicator.OtherPlayerData.PlayerWorldPosition;
+                Visible = true;
+            }
+        }
+
+        private void CheckForRestart(int millisecondsSinceLastUpdate)
+        {
+            if (_millisecondsToRestart > 0)
+            {
+                _millisecondsToRestart -= millisecondsSinceLastUpdate;
+
+                if (_millisecondsToRestart < 1)
+                {
+                    Visible = true;
+                    StartMovement();
+                }
+            }
+        }
+
+        private void UpdateClientVelocity()
+        {
+            if (_peerVelocity.X != 0.0f)
+            {
+                if (_clientVelocity.X == 0)
+                {
+                    StartMovement();
+                }
+
+                _clientVelocity = (_clientVelocity * 0.5f) + (_peerVelocity * 0.5f);
             }
         }
 
--- Bopscotch/Bopscotch/Scenes/Gameplay/Race/RaceGameplayScene.cs
+++ Bopscotch/Bopscotch/Scenes/Gameplay/Race/RaceGameplayScene.cs
@@ -158,6 +158,7 @@ namespace Bopscotch.Scenes.Gameplay.Race
             }
             else 
             {
+                _opponentMarker.StartMovement();
                 _raceStarted = true;
 
                 SoundEffectManager.PlayEffect("race-start"); 
@@ -246,7 +247,7 @@ namespace Bopscotch.Scenes.Gameplay.Race
                 _player.PlayerEventCallback = HandlePlayerEvent;
                 ((PlayerMotionEngine)_player.MotionEngine).DifficultySpeedBoosterUnit = NextSceneParameters.Get<int>(Course_Speed_Parameter);
 
-                _opponentMarker.SetForRaceStart(_player.WorldPosition);
+                _opponentMarker.SetForRaceStart(_player.WorldPosition, _player.Mirror);
                 SetCoordinatorsForRace(NextSceneParameters.Get<string>(Course_Area_Parameter));
                 SetUpOpponentAttackEffects();
                 _waitingMessage.Activate();
