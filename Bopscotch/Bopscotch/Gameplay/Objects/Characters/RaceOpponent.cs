93ac1c4333f6ef3fca5385f7e2df16a7cf0d4070
Ben-P-Leda <benp@ledaentertainment.co.uk>
2016-01-07 06:43:56 +0000

    Capture average lag

--- Bopscotch/Bopscotch/Gameplay/Objects/Characters/RaceOpponent.cs
+++ Bopscotch/Bopscotch/Gameplay/Objects/Characters/RaceOpponent.cs
@@ -14,21 +14,25 @@ namespace Bopscotch.Gameplay.Objects.Characters
 {
     public class RaceOpponent : DisposableSkeleton, ICameraRelative, IMobile
     {
-        private long _millisecondsSinceLastComms;
+        private long _millisecondsSinceLastPacket;
         private Vector2 _velocity;
         private Vector2 _physicalPosition;
         private Vector2 _difference;
         private int _fadeDirection;
         private float _fadeFraction;
+        private float[] _previousCommsLagTimes;
+        private int _earliestCommsLagSlot;
+        private float _averageCommsLagTime;
 
         public InterDeviceCommunicator Communicator { private get; set; }
         public IMotionEngine MotionEngine { get { return null; } }
         public AdditiveLayerParticleEffectManager ParticleManager { private get; set; }
 
-
         public RaceOpponent()
             : base()
         {
+            _previousCommsLagTimes = new float[Comms_Sample_Count];
+
             RenderLayer = 2;
             WorldPositionIsFixed = false;
             Scale = 0.9f;
@@ -37,12 +41,12 @@ namespace Bopscotch.Gameplay.Objects.Characters
 
         public override void Reset()
         {
-            _millisecondsSinceLastComms = 0;
             _velocity = Vector2.Zero;
             _physicalPosition = Vector2.Zero;
             _difference = Vector2.Zero;
             _fadeDirection = 1;
             _fadeFraction = 0.0f;
+            _millisecondsSinceLastPacket = 0;
 
             WorldPosition = Vector2.Zero;
             Visible = true;
@@ -56,6 +60,8 @@ namespace Bopscotch.Gameplay.Objects.Characters
             SkinBones(AvatarComponentManager.SideFacingAvatarSkin(Communicator.OtherPlayerAvatarSlot));
             Rectangle bounds = TextureManager.Textures["race-p2-cloud"].Bounds;
             Bones["cloud"].ApplySkin("race-p2-cloud", new Vector2(bounds.Width, bounds.Height) * 0.5f, bounds);
+
+            ResetAverageLagTime();
         }
 
         private StorableBone CreateCloudBone()
@@ -68,7 +74,42 @@ namespace Bopscotch.Gameplay.Objects.Characters
             return cloud;
         }
 
+        private void ResetAverageLagTime()
+        {
+            for (int i=0; i<Comms_Sample_Count; i++)
+            {
+                _previousCommsLagTimes[i] = InterDeviceCommunicator.Default_Send_Interval;
+            }
+
+            _earliestCommsLagSlot = 0;
+            _averageCommsLagTime = InterDeviceCommunicator.Default_Send_Interval;
+        }
+
         public void Update(int millisecondsSinceLastUpdate)
+        {
+            if (Communicator.MillisecondsSinceLastReceive < _millisecondsSinceLastPacket)
+            {
+                HandlePacketReceived();
+            }
+            _millisecondsSinceLastPacket = Communicator.MillisecondsSinceLastReceive;
+
+            UpdateDisplaySettings();
+        }
+
+        private void HandlePacketReceived()
+        {
+            _previousCommsLagTimes[_earliestCommsLagSlot] = _millisecondsSinceLastPacket;
+            _earliestCommsLagSlot = (_earliestCommsLagSlot + 1) % Comms_Sample_Count;
+
+            _averageCommsLagTime = 0;
+            for (int i=0; i<Comms_Sample_Count; i++)
+            {
+                _averageCommsLagTime += _previousCommsLagTimes[i];
+            }
+            _averageCommsLagTime /= Comms_Sample_Count;
+        }
+
+        private void UpdateDisplaySettings()
         {
             WorldPosition = Communicator.OtherPlayerData.PlayerWorldPosition;
 
@@ -120,5 +161,6 @@ namespace Bopscotch.Gameplay.Objects.Characters
 
         private const float Fade_Step = 0.01f;
         private const float Position_Lock_Distance = 250.0f;
+        private const int Comms_Sample_Count = 20;
     }
 }
